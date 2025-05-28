43266e0efa3adb9073f6d07a5e7d9ef697dfdf2b
myrrhax <sizintsevdenis05@mail.ru>
2025-05-28 20:51:46 +0300

    Article entity

---
+++ src/Backend/Domain/Entities/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class Article
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public Dictionary<string, object> Content { get; set; } = [];
+    public DateTime CreationDate { get; set; }
+}
