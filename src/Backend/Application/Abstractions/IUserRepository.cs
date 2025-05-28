86a9c48efe803e9f48f37bf233c11b8b1e40d632
myrrhax <sizintsevdenis05@mail.ru>
2025-05-28 22:20:30 +0300

    user repository abstraction

---
+++ src/Backend/Application/Abstractions/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Utils;
+
+namespace Application.Abstractions;
+
+public interface IUserRepository
+{
+    Task<Result<ApplicationUser>> GetUserInfoById(Guid id, CancellationToken cancellationToken);
+    Task<Result<ApplicationUser>> GetUserById(Guid id, CancellationToken cancellationToken);
+    Task<Result<ApplicationUser>> GetUserByLogin(string login, CancellationToken cancellationToken);
+    Task<Result> AddUser(ApplicationUser user, CancellationToken cancellationToken);
+    Task<Result> UpdateUser(ApplicationUser user, CancellationToken cancellationToken);
+    Task<Result> DeleteUserById(Guid userId, CancellationToken cancellationToken);
+    Task<Result> AddRefreshToken(RefreshToken refreshToken, CancellationToken cancellationToken);
+    Task<Result<RefreshToken>> GetRefreshToken(string token, Guid userId, CancellationToken cancellationToken);
+    Task<Result> UpdateRefreshToken(RefreshToken token, CancellationToken cancellationToken);
+    Task<Result> DeleteRefreshToken(string token, Guid userId, CancellationToken cancellationToken);
+}
