f0ce67553db534e9396971e637fdf47e607eea33
myrrhax <sizintsevdenis05@mail.ru>
2025-05-28 21:12:14 +0300

    Article user references

--- src/Backend/Domain/Entities/Article.cs
+++ src/Backend/Domain/Entities/Article.cs
@@ -5,5 +5,8 @@ public class Article
     public required string Id { get; set; }
     public required string Title { get; set; }
     public Dictionary<string, object> Content { get; set; } = [];
+    public Guid CreatorId { get; set; }
+    public required string CreatorUserName { get; set; }
+    public required string CreatorAvatarUrl { get; set; }
     public DateTime CreationDate { get; set; }
 }
