82606d8291fd67ab34de73350e1b5eda3eaf773e
myrrhax <sizintsevdenis05@mail.ru>
2025-05-28 19:50:00 +0300

    Application user entity

---
+++ src/Backend/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities;
+
+public class ApplicationUser
+{
+    public Guid Id { get; set; }
+    public string DisplayedName { get; set; } = string.Empty;
+    public string Login { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateOnly BirthDate { get; set; }
+    public DateTime RegistrationTime { get; set; }
+}
