d9a5d1ae42584150b15e91424d789922915803ac
myrrhax <sizintsevdenis05@mail.ru>
2025-05-29 19:42:46 +0300

    implement AddUser

--- src/Backend/Infrastructure/Implementation/UserRepositoryImpl.cs
+++ src/Backend/Infrastructure/Implementation/UserRepositoryImpl.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Abstractions;
 using Domain.Entities;
 using Domain.Utils;
+using Domain.Utils.Errors;
 using Infrastructure.DataContext;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Implementation;
@@ -14,22 +16,45 @@ internal class UserRepositoryImpl(ApplicationDbContext context, ILogger<UserRepo
         {
             await context.RefreshTokens.AddAsync(refreshToken, cancellationToken);
             await context.SaveChangesAsync(cancellationToken);
+            logger.LogInformation("New refresh token was added to user with id: {}", refreshToken.UserId);
 
             return Result.Success();
         }
-        catch (Exception)
+        catch (Exception ex)
         {
+            logger.LogError("Unable to add new refresh token to user with id: {}.\nError message: {}\n.Stack trace: {}",
+                refreshToken.UserId, ex.Message, ex.StackTrace);
+            return Result.Failure(new InsertionError(string.Empty));
         }
     }
 
-    public Task<Result> AddUser(ApplicationUser user, CancellationToken cancellationToken)
+    public async Task<Result> AddUser(ApplicationUser user, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        try
+        {
+            await context.Users.AddAsync(user, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+            logger.LogInformation("New user was added to database with id: {}", user.Id);
+
+            return Result.Success();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Unable to add new user to database.\nError message: {}\n. Stack trace: {}",
+                ex.Message, ex.StackTrace);
+            return Result.Failure(new InsertionError(string.Empty));
+        }
     }
 
-    public Task<Result> DeleteRefreshToken(RefreshToken token, CancellationToken cancellationToken)
+    public async Task<Result> DeleteRefreshToken(RefreshToken token, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        int rows = await context.RefreshTokens
+            .Where(entity => entity.Id == token.Id)
+            .ExecuteDeleteAsync(cancellationToken);
+
+        return rows > 0
+            ? Result.Success()
+            : Result.Failure();
     }
 
     public Task<Result> DeleteUserById(Guid userId, CancellationToken cancellationToken)
