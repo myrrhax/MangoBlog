7ec6bf2618107a7f0c97696cc2a2231c7424125b
myrrhax <sizintsevdenis05@mail.ru>
2025-05-29 19:17:03 +0300

    users configuration

---
+++ src/Backend/Infrastructure/DataContext/Configuration/UsersConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DataContext.Configuration;
+
+internal class UsersConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.HasKey(entity => entity.Id);
+
+        builder.Property(entity => entity.Login)
+            .HasMaxLength(25)
+            .IsRequired();
+        builder.HasIndex(entity => entity.Login)
+            .IsUnique();
+
+        builder.Property(entity => entity.Email)
+            .IsRequired();
+        builder.HasIndex(entity => entity.Email)
+            .IsUnique();
+
+        builder.Property(entity => entity.DisplayedName)
+            .IsRequired();
+
+        builder.Property(entity => entity.FirstName)
+            .HasMaxLength(25)
+            .IsRequired();
+
+        builder.Property(entity => entity.LastName)
+            .HasMaxLength(25)
+            .IsRequired();
+
+        builder.Property(entity => entity.AvatarUrl)
+            .HasDefaultValue(null);
+
+        builder.HasMany(user => user.RefreshTokens)
+            .WithOne(token => token.User)
+            .HasForeignKey(token => token.Id);
+
+        builder.HasMany(user => user.Subscriptions)
+            .WithMany();
+    }
+}
