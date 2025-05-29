b49f2229fdb35aa13083b564330675612ebc8f84
myrrhax <sizintsevdenis05@mail.ru>
2025-05-29 19:25:30 +0300

    ratings configuration

---
+++ src/Backend/Infrastructure/DataContext/Configuration/RatingsConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DataContext.Configuration;
+
+public class RatingsConfiguration : IEntityTypeConfiguration<Rating>
+{
+    public void Configure(EntityTypeBuilder<Rating> builder)
+    {
+        builder.HasKey(entity => new { entity.UserId, entity.ArticleId });
+
+        builder.Property(entity => entity.RatingType)
+            .IsRequired()
+            .HasConversion<string>();
+    }
+}
