8c0f7552e1f8d2f7bcedb2356a9a06bb2754c255
myrrhax <sizintsevdenis05@mail.ru>
2025-05-29 19:09:51 +0300

    db context

---
+++ src/Backend/Infrastructure/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataContext;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<ApplicationUser> Users { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
