998b748f0d7b3c15630759a41cf85601fde39d4d
myrrhax <sizintsevdenis05@mail.ru>
2025-05-30 17:01:28 +0300

    di registration

--- src/Backend/WebApi/Program.cs
+++ src/Backend/WebApi/Program.cs
@@ -1,8 +1,24 @@
+using Infrastructure;
+using Application.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Register Infrastructure
+builder.Services
+    .AddDatabase(builder.Configuration.GetConnectionString("postgres")!)
+    .AddServices()
+    .AddRepositories();
+#endregion
+
+#region Register Application
+builder.Services
+    .AddValidation()
+    .AddUseCases();
+#endregion
 
 var app = builder.Build();
 
