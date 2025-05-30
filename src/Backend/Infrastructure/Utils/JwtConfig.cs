25728e53585cea5301e7a4137867cfba33d200ed
myrrhax <sizintsevdenis05@mail.ru>
2025-05-30 17:32:31 +0300

    add audience validation

--- src/Backend/Infrastructure/Utils/JwtConfig.cs
+++ src/Backend/Infrastructure/Utils/JwtConfig.cs
@@ -4,6 +4,7 @@ public class JwtConfig
 {
     public string Issuer { get; set; } = string.Empty;
     public string SecretKey { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
     public int ExpirationTimeMinutes { get; set; }
     public int RefreshTokenExpirationDays { get; set; }
     public int RefreshTokenMaxSessionsCount { get; set; }
