b1de689fbc922fa93a2dd320f4c0d61376cb27d9
myrrhax <sizintsevdenis05@mail.ru>
2025-05-30 17:21:18 +0300

    users controller

---
+++ src/Backend/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dto;
+using Application.Users.Commands;
+using Domain.Utils;
+using Domain.Utils.Errors;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> RegisterUser([FromBody] RegisterUserCommand request)
+    {
+        Result<RegistrationResponse> result = await mediator.Send(request);
+
+        return result.IsSuccess
+            ? Ok(result.Value)
+            : BadRequest(result.Error);
+    }
+}
