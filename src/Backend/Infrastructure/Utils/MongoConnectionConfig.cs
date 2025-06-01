1d63531af8c9a957d7984116345e434bd77eb2e5
myrrhax <sizintsevdenis05@mail.ru>
2025-06-01 18:03:15 +0300

    di for mongo

---
+++ src/Backend/Infrastructure/Utils/MongoConnectionConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Utils;
+
+public class MongoConnectionConfig
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+}
