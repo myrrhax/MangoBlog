8ff5c2544de2355cfeb24c6544b9834039ae0637
myrrhax <sizintsevdenis05@mail.ru>
2025-06-01 20:49:36 +0300

    add newtonsoft json for better parsing

--- src/Backend/WebApi/Program.cs
+++ src/Backend/WebApi/Program.cs
@@ -5,7 +5,7 @@ using Infrastructure.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddJwtAuthentication(builder.Configuration);
