0218f083e2156a154a65f97264afbaaccc4f080d
myrrhax <sizintsevdenis05@mail.ru>
2025-06-01 23:44:51 +0300

    logic for get my articles

--- src/Backend/Application/Abstractions/IRatingsRepository.cs
+++ src/Backend/Application/Abstractions/IRatingsRepository.cs
@@ -10,4 +10,5 @@ public interface IRatingsRepository
     Task<double> GetPostAverageRating(string postId, CancellationToken cancellationToken);
     Task<RatingType?> GetUserRating(Guid userId, string postId, CancellationToken cancellationToken);
     Task<IEnumerable<Rating>> GetUserRatings(Guid userId, CancellationToken cancellationToken);
+    Task<Dictionary<string, (int, int)>> GetRatingsForArticles(IEnumerable<Article> articles, CancellationToken cancellationToken);
 }
--- src/Backend/Application/Articles/Queries/GetMyArticlesQuery.cs
+++ src/Backend/Application/Articles/Queries/GetMyArticlesQuery.cs
@@ -1,22 +1,55 @@
 ﻿using Application.Abstractions;
+using Application.Dto.Articles;
+using Application.Extentions;
 using Domain.Entities;
 using MediatR;
 
 namespace Application.Articles.Queries;
 
-public record GetMyArticlesQuery(Guid UserId) : IRequest<IEnumerable<Article>>;
+public record GetMyArticlesQuery(Guid UserId) : IRequest<IEnumerable<ArticleDto>>;
 
-public class GetMyArticlesQueryHandler : IRequestHandler<GetMyArticlesQuery, IEnumerable<Article>>
+public class GetMyArticlesQueryHandler : IRequestHandler<GetMyArticlesQuery, IEnumerable<ArticleDto>>
 {
     private readonly IArticlesRepository _articlesRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IRatingsRepository _ratingsRepository;
 
-    public GetMyArticlesQueryHandler(IArticlesRepository articlesRepository)
+    public GetMyArticlesQueryHandler(IArticlesRepository articlesRepository, IUserRepository userRepository, IRatingsRepository ratingsRepository)
     {
         _articlesRepository = articlesRepository;
+        _userRepository = userRepository;
+        _ratingsRepository = ratingsRepository;
     }
 
-    public Task<IEnumerable<Article>> Handle(GetMyArticlesQuery request, CancellationToken cancellationToken)
+    public async Task<IEnumerable<ArticleDto>> Handle(GetMyArticlesQuery request, CancellationToken cancellationToken)
     {
-        
+        Task<IEnumerable<Article>> getArticlesTask = _articlesRepository.GetUserArticles(request.UserId);
+        Task<ApplicationUser?> getUserTask = _userRepository.GetUserById(request.UserId, cancellationToken);
+
+        await Task.WhenAll(getArticlesTask, getUserTask);
+        IEnumerable<Article> articles = getArticlesTask.Result;
+        ApplicationUser? user = getUserTask.Result;
+
+        if (!articles.Any() || user is null)
+            return Enumerable.Empty<ArticleDto>();
+
+        Dictionary<string, (int, int)> ratings = await _ratingsRepository.GetRatingsForArticles(articles, cancellationToken);
+        return articles.Select(article =>
+        {
+            int likes = 0, dislikes = 0;
+            if (ratings.ContainsKey(article.Id))
+            {
+                (likes, dislikes) = ratings[article.Id];
+            }
+
+            return new ArticleDto(article.Id,
+                user.MapToDto(),
+                article.Content,
+                article.Tags,
+                article.CreationDate,
+                likes,
+                dislikes,
+                UserRating: null);
+        });
     }
 }
