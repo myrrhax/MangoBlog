e4116ac23c3b880853cff75e1be3761d245e1d4f
myrrhax <sizintsevdenis05@mail.ru>
2025-06-01 21:30:21 +0300

    i rating repository abstraction

---
+++ src/Backend/Application/Abstractions/IRatingsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Application.Abstractions;
+
+public interface IRatingsRepository
+{
+    Task<int> GetPostLikesCount(Guid postId, CancellationToken cancellationToken);
+    Task<int> GetPostDislikesCount(Guid postId, CancellationToken cancellationToken);
+    Task<double> GetPostAverageRating(Guid postId, CancellationToken cancellationToken);
+    Task<RatingType?> GetUserRating(Guid userId, CancellationToken cancellationToken);
+}
--- src/Backend/Application/Articles/Queries/GetArticleByIdQuery.cs
+++ src/Backend/Application/Articles/Queries/GetArticleByIdQuery.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Abstractions;
 using Application.Dto.Articles;
+using Application.Extentions;
 using Domain.Entities;
 using MediatR;
 
@@ -10,14 +11,25 @@ public record GetArticleByIdQuery(string ArticleId) : IRequest<ArticleDto?>;
 public class GetArticleByIdQueryHandler : IRequestHandler<GetArticleByIdQuery, ArticleDto?>
 {
     private readonly IArticlesRepository _articlesRepository;
+    private readonly IUserRepository _userRepository;
 
-    public GetArticleByIdQueryHandler(IArticlesRepository articlesRepository)
+    public GetArticleByIdQueryHandler(IArticlesRepository articlesRepository, IUserRepository userRepository)
     {
         _articlesRepository = articlesRepository;
+        _userRepository = userRepository;
     }
 
-    public Task<ArticleDto?> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
+    public async Task<ArticleDto?> Handle(GetArticleByIdQuery request, CancellationToken cancellationToken)
     {
-        Article? article = 
+        Article? article = await _articlesRepository.GetArticleById(request.ArticleId);
+        
+        if (article is null)
+            return null;
+
+        ApplicationUser? creator = await _userRepository.GetUserById(article.CreatorId, cancellationToken);
+        if (creator is null)
+            return null;
+
+
     }
 }
