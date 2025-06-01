203f9bc870396655551adbe7d89104deac460e8b
myrrhax <sizintsevdenis05@mail.ru>
2025-06-01 23:30:02 +0300

    query + handler

---
+++ src/Backend/Application/Articles/Queries/GetMyArticlesQuery.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Articles.Queries;
+
+public record GetMyArticlesQuery(Guid UserId) : IRequest<IEnumerable<Article>>;
+
+public class GetMyArticlesQueryHandler : IRequestHandler<GetMyArticlesQuery, IEnumerable<Article>>
+{
+    private readonly IArticlesRepository _articlesRepository;
+
+    public GetMyArticlesQueryHandler(IArticlesRepository articlesRepository)
+    {
+        _articlesRepository = articlesRepository;
+    }
+
+    public Task<IEnumerable<Article>> Handle(GetMyArticlesQuery request, CancellationToken cancellationToken)
+    {
+        
+    }
+}
