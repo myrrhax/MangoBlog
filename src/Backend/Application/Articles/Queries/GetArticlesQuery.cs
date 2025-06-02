2e82e35deacbb7509bc27bdc5dedb4d1812c5972
myrrhax <sizintsevdenis05@mail.ru>
2025-06-02 19:23:55 +0300

    get articles query

---
+++ src/Backend/Application/Articles/Queries/GetArticlesQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dto.Articles;
+using MediatR;
+
+namespace Application.Articles.Queries;
+
+public record GetArticlesQuery(IEnumerable<string> Tags,
+    string Query = "",
+    string SortByDate = "",
+    string SortByPopularity = "",
+    Guid? AuthorId = null) : IRequest<IEnumerable<ArticleDto>>;
