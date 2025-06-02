836695cf0f7bef204d6aefd6ac49b0a11a688b3d
myrrhax <sizintsevdenis05@mail.ru>
2025-06-02 16:30:28 +0300

    command for update article

---
+++ src/Backend/Application/Articles/Commands/UpdateArticleCommand.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dto.Articles;
+using Domain.Utils;
+using MediatR;
+
+namespace Application.Articles.Commands;
+
+public record UpdateArticleCommand(string ArticleId, 
+    Guid CallerId,
+    string Title,
+    Dictionary<string, object> Content, 
+    IEnumerable<string> Tags) : IRequest<Result<ArticleDto>>;
---
+++ src/Backend/Application/Dto/Articles/UpdateArticleDto.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Dto.Articles;
+
+public record UpdateArticleDto(string ArticleId, 
+    string Title, 
+    Dictionary<string, object> Content, 
+    IEnumerable<Tag> Tags);
