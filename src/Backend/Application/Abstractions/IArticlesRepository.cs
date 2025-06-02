06261e5e131b3f688e2780ae92d78ff54e1657c2
myrrhax <sizintsevdenis05@mail.ru>
2025-06-02 16:31:10 +0300

    update method interface

--- src/Backend/Application/Abstractions/IArticlesRepository.cs
+++ src/Backend/Application/Abstractions/IArticlesRepository.cs
@@ -7,6 +7,7 @@ namespace Application.Abstractions;
 public interface IArticlesRepository
 {
     Task<Result<Article>> CreateArticle(CreateArticleDto dto);
+    Task<Result<Article>> UpdateArticle(UpdateArticleDto dto);
     Task<Article?> GetArticleById(string id);
     Task<IEnumerable<Article>> GetUserArticles(Guid userId);
     Task<Result> DeleteArtcile(string artcileId);
