8bc69d74995aaf0bacfbf5da39eb43ae9a8c4293
myrrhax <sizintsevdenis05@mail.ru>
2025-06-02 19:36:11 +0300

    validator

--- src/Backend/Application/Articles/Queries/GetArticlesQuery.cs
+++ src/Backend/Application/Articles/Queries/GetArticlesQuery.cs
@@ -1,10 +1,30 @@
-﻿using Application.Dto.Articles;
+﻿using Application.Abstractions;
+using Application.Dto.Articles;
+using FluentValidation;
 using MediatR;
 
 namespace Application.Articles.Queries;
 
-public record GetArticlesQuery(IEnumerable<string> Tags,
-    string Query = "",
-    string SortByDate = "",
-    string SortByPopularity = "",
-    Guid? AuthorId = null) : IRequest<IEnumerable<ArticleDto>>;
+public record GetArticlesQuery(IEnumerable<string>? Tags = null,
+    string? Query = null,
+    string? SortByDate = null,
+    string? SortByPopularity = null,
+    Guid? AuthorId = null,
+    int Page = 1) : IRequest<IEnumerable<ArticleDto>>;
+
+public class GetArticlesQueryHandler : IRequestHandler<GetArticlesQuery, IEnumerable<ArticleDto>>
+{
+    private readonly IArticlesRepository _articleRepository;
+    private readonly IValidator<GetArticlesQuery> _validator;
+
+    public GetArticlesQueryHandler(IArticlesRepository articleRepository, IValidator<GetArticlesQuery> validator)
+    {
+        _articleRepository = articleRepository;
+        _validator = validator;
+    }
+
+    public Task<IEnumerable<ArticleDto>> Handle(GetArticlesQuery request, CancellationToken cancellationToken)
+    {
+        
+    }
+}
---
+++ src/Backend/Application/Validation/GetArticlesQueryValidator.cs
@@ -0,0 +1,33 @@
+﻿using Application.Articles.Queries;
+using FluentValidation;
+
+namespace Application.Validation;
+
+public class GetArticlesQueryValidator : AbstractValidator<GetArticlesQuery>
+{
+    public static readonly string[] ValidSortTypes = ["asc", "desc", ""];
+    public GetArticlesQueryValidator()
+    {
+        RuleFor(article => article.Page)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Page must be greater than 1");
+
+        RuleFor(article => article.AuthorId)
+            .Must(id => id != Guid.Empty)
+            .When(article => article.AuthorId is not null)
+            .WithMessage("Invalid author id format. Id must be Guid.");
+
+        RuleFor(article => article.SortByDate)
+            .Must(IsSortTypeValid!)
+            .When(article => article.SortByDate is not null)
+            .WithMessage("Invalid sort type. Valid types: {asc, desc, ''}");
+
+        RuleFor(article => article.SortByPopularity)
+            .Must(IsSortTypeValid!)
+            .When(article => article.SortByPopularity is not null)
+            .WithMessage("Invalid sort type. Valid types: {asc, desc, ''}");
+    }
+
+    private bool IsSortTypeValid(string type)
+        => ValidSortTypes.Contains(type.ToLower());
+}
