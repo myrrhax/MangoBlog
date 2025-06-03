c307a0962e49dffff9d72bf220b7d3e648928491
myrrhax <sizintsevdenis05@mail.ru>
2025-06-03 18:22:33 +0300

    add rating command

---
+++ src/Backend/Application/Users/Commands/AddRatingToPostCommand.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Enums;
+using Domain.Utils;
+using MediatR;
+
+namespace Application.Users.Commands;
+
+public record AddRatingToPostCommand(string PostId, 
+    Guid CallerId, 
+    RatingType Type) : IRequest<Result>;
