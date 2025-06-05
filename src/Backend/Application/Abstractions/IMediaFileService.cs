e8ba51262885f64e3883f5fecae7970a6dc22ab8
myrrhax <sizintsevdenis05@mail.ru>
2025-06-05 18:13:53 +0300

    query + query handler

--- src/Backend/Application/Abstractions/IMediaFileService.cs
+++ src/Backend/Application/Abstractions/IMediaFileService.cs
@@ -6,18 +6,10 @@ namespace Application.Abstractions;
 
 public interface IMediaFileService
 {
-    /// <summary>
-    /// Method loads file and returns hashname of file
-    /// </summary>
-    /// <param name="fileStream">Input file stream</param>
-    /// <param name="extention">File extentions (jpeg, mp4, e.t.c.)</param>
-    /// <param name="creatorId">Creator user id</param>
-    /// <param name="isAvatar">Crops image if true</param>
-    /// <returns>File hashname</returns>
     Task<Result<MediaFile>> LoadFileToServer(Stream fileStream, 
         string extention,
         Guid creatorId, 
         bool isAvatar);
-    Task<Stream?> LoadFile(string fileName);
-    Task<MediaFile?> GetMediaFile(string fileName);
+    Task<Stream?> LoadFile(Guid fileId);
+    Task<MediaFile?> GetMediaFile(Guid fileName);
 }
---
+++ src/Backend/Application/MediFiles/Queries/GetMediaByIdQuery.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.MediFiles.Queries;
+
+public record GetMediaByIdQuery(Guid MediaId) : IRequest<Stream?>;
+
+public class GetMediaByIdQueryHandler : IRequestHandler<GetMediaByIdQuery, Stream?>
+{
+    private readonly IMediaFileService _mediaFileService;
+
+    public GetMediaByIdQueryHandler(IMediaFileService mediaFileService)
+    {
+        _mediaFileService = mediaFileService;
+    }
+
+    public async Task<Stream?> Handle(GetMediaByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _mediaFileService.LoadFile(request.MediaId);
+    }
+}
