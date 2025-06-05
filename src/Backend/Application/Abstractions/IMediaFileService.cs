8456626f3671a991c15bfc941f7752742acecb10
myrrhax <sizintsevdenis05@mail.ru>
2025-06-05 15:08:31 +0300

    media file service interface

---
+++ src/Backend/Application/Abstractions/IMediaFileService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enums;
+using Domain.Utils;
+
+namespace Application.Abstractions;
+
+public interface IMediaFileService
+{
+    Task<Result<string>> LoadFileToServer(Stream fileStream, MediaFileType type);
+    Task<Stream?> GetFileByHashName(string hashName);
+}
