f1a3616f12464ed90b0b0d09d55239bf1b4cfc6b
myrrhax <sizintsevdenis05@mail.ru>
2025-06-05 15:02:01 +0300

    media file configuration

---
+++ src/Backend/Infrastructure/DataContext/Configuration/MediaFileConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DataContext.Configuration;
+
+internal class MediaFileConfiguration : IEntityTypeConfiguration<MediaFile>
+{
+    public void Configure(EntityTypeBuilder<MediaFile> builder)
+    {
+        builder.HasKey(file => file.Id);
+
+        builder.Property(file => file.HashName)
+            .IsRequired();
+        builder.HasIndex(file => file.HashName)
+            .IsUnique();
+
+        builder.Property(file => file.FilePath)
+            .IsRequired();
+        builder.HasIndex(file => file.FilePath)
+            .IsUnique();
+
+        builder.HasOne(file => file.Loader)
+            .WithMany(loader => loader.MediaFiles)
+            .HasForeignKey(file => file.LoaderId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
