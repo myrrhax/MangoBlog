134c43365c106d26cb18b7d55bf1f7781e06944e
myrrhax <sizintsevdenis05@mail.ru>
2025-06-06 15:07:50 +0300

    add integrations info in dto

---
+++ src/Backend/Application/Dto/IntegrationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Dto;
+
+public class IntegrationDto
+{
+    public string IntegrationType { get; set; } = string.Empty;
+    public string AccountId { get; set; } = string.Empty;
+    public bool IsConfirmed { get; set; }
+    public IEnumerable<string> ConnectedRooms { get; set; } = [];
+}
