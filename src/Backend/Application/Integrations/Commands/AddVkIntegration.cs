6cbd751536cf303e3f854839dd11126b800583dd
myrrhax <sizintsevdenis05@mail.ru>
2025-06-06 15:26:12 +0300

    vk integration command

---
+++ src/Backend/Application/Integrations/Commands/AddVkIntegration.cs
@@ -0,0 +1,9 @@
+﻿using Application.Dto;
+using Domain.Utils;
+using MediatR;
+
+namespace Application.Integrations.Commands;
+
+public record AddVkIntegration(Guid CallerId, 
+    string ApiToken,
+    string GroupId) : IRequest<Result<IntegrationDto>>;
