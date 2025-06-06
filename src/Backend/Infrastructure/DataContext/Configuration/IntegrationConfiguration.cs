e45b934df3484d57daeb73351a4c2db970db8d09
myrrhax <sizintsevdenis05@mail.ru>
2025-06-06 14:47:36 +0300

    configuration for integration

---
+++ src/Backend/Infrastructure/DataContext/Configuration/IntegrationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DataContext.Configuration;
+
+internal class IntegrationConfiguration : IEntityTypeConfiguration<Integration>
+{
+    public void Configure(EntityTypeBuilder<Integration> builder)
+    {
+        builder.HasKey(integration => integration.Id);
+
+        builder.Property(integration => integration.IntegrationType)
+            .IsRequired()
+            .HasConversion<string>();
+        builder.HasIndex(integration => integration.IntegrationType)
+            .IsUnique();
+
+        builder.HasData([
+            new Integration { Id = 1, IntegrationType = IntegrationType.Telegram },
+            new Integration { Id = 2, IntegrationType = IntegrationType.VKontakte },
+        ]);
+    }
+}
