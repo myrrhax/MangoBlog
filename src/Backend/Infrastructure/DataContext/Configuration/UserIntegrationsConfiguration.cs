dab102ae03fd538a1df7bd77b5e184d0b2efe811
myrrhax <sizintsevdenis05@mail.ru>
2025-06-06 14:51:46 +0300

    UserIntegration entity configuration

---
+++ src/Backend/Infrastructure/DataContext/Configuration/UserIntegrationsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.DataContext.Configuration;
+
+internal class UserIntegrationsConfiguration : IEntityTypeConfiguration<UserIntegration>
+{
+    public void Configure(EntityTypeBuilder<UserIntegration> builder)
+    {
+        builder.HasKey(userIntegration => new { userIntegration.UserId, userIntegration.IntegrationId });
+
+        builder.HasOne(userIntegration => userIntegration.User)
+            .WithMany(userIntegration => userIntegration.Integrations)
+            .HasForeignKey(userIntegration => userIntegration.IntegrationId);
+
+        builder.HasOne(userIntegration => userIntegration.Integration)
+            .WithMany();
+
+        builder.HasMany(userIntegration => userIntegration.ConnectedRooms)
+            .WithOne(connectedRoom => connectedRoom.UserIntegration);
+    }
+}
