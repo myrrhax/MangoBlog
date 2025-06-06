57045567599d35e9b86ece47014be13c602e1f53
myrrhax <sizintsevdenis05@mail.ru>
2025-06-06 15:30:00 +0300

    integration repository interface

---
+++ src/Backend/Application/Abstractions/IIntegrationRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Domain.Utils;
+
+namespace Application.Abstractions;
+
+public interface IIntegrationRepository
+{
+    Task<Result> AddIntegration(UserIntegration integration, CancellationToken cancellationToken);
+    Task<Result> ConfirmIntegration(Guid UserId, IntegrationType type, CancellationToken cancellationToken);
+}
