575116655a6a640166be7a3cce4df430cd41a02f
myrrhax <sizintsevdenis05@mail.ru>
2025-06-07 15:09:51 +0300

    publication entity

---
+++ src/Backend/Domain/Entities/Publication.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Publication
+{
+    public string PublicationId { get; set; } = string.Empty;
+    public Guid UserId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public List<string> ImagesId { get; set; } = [];
+    public List<IntegrationType> PublishedAt { get; set; } = [];
+    public Dictionary<IntegrationType, List<string>> PublishedPostIds { get; set; } = [];
+    public DateTime CreationDate { get; set; }
+    public DateTime PublicationTime { get; set; }
+    public bool IsPublished { get; set; }
+}
