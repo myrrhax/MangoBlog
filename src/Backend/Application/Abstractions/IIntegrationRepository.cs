a243f3f0100f375e2c9635dd4391cd5e66c45e74
myrrhax <sizintsevdenis05@mail.ru>
2025-06-08 19:32:29 +0300

    confirm telegram integration logic

--- src/Backend/Application/Abstractions/IIntegrationRepository.cs
+++ src/Backend/Application/Abstractions/IIntegrationRepository.cs
@@ -10,7 +10,7 @@ public interface IIntegrationRepository
     Task<Integration?> GetIntegrationByTelegramId(string telegramId, CancellationToken cancellationToken);
     Task<TelegramIntegration?> GetTelegramIntegration(Guid userId, CancellationToken cancellationToken);
     Task<Result> AddTelegramIntegration(TelegramIntegration integration, CancellationToken cancellationToken);
-    Task<Result> ConfirmTelegramIntegration(string integrationCode, CancellationToken cancellationToken);
+    Task<Result<Integration>> ConfirmTelegramIntegration(string integrationCode, string telegramId, CancellationToken cancellationToken);
     Task<Result> DeleteTelegramIntegration(Guid userId, CancellationToken cancellationToken);
     Task<Result> DeleteFromTelegramChannel(Guid userId, string channelId, CancellationToken cancellationToken);
 }
---
+++ src/Backend/Application/Integrations/Commands/ConfirmTelegramIntegrationCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.Abstractions;
+using Application.Dto.Integrations;
+using Application.Extentions;
+using Domain.Entities;
+using Domain.Utils;
+using MediatR;
+
+namespace Application.Integrations.Commands;
+
+public record ConfirmTelegramIntegrationCommand(string IntegrationCode, 
+    string TelegramId) : IRequest<Result<IntegrationDto>>;
+
+public class ConfirmTelegramIntegrationCommandHandler : IRequestHandler<ConfirmTelegramIntegrationCommand, Result<IntegrationDto>>
+{
+    private readonly IIntegrationRepository _integrationRepository;
+
+    public ConfirmTelegramIntegrationCommandHandler(IIntegrationRepository integrationRepository)
+    {
+        _integrationRepository = integrationRepository;
+    }
+
+    public async Task<Result<IntegrationDto>> Handle(ConfirmTelegramIntegrationCommand request, CancellationToken cancellationToken)
+    {
+        Result<Integration> confirmResult = await _integrationRepository.ConfirmTelegramIntegration(request.IntegrationCode, request.TelegramId, cancellationToken);
+        if (confirmResult.IsFailure)
+            return Result.Failure<IntegrationDto>(confirmResult.Error);
+
+        return Result.Success(confirmResult.Value!.MapToDto());
+    }
+}
