2812bf750bec1976b5ec8e4aeb0a567c787261aa
myrrhax <sizintsevdenis05@mail.ru>
2025-06-08 19:16:31 +0300

    get integration info by telegram id query + handler + interface

--- src/Backend/Application/Abstractions/IIntegrationRepository.cs
+++ src/Backend/Application/Abstractions/IIntegrationRepository.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Application.Dto;
+using Domain.Entities;
 using Domain.Enums;
 using Domain.Utils;
 
@@ -6,7 +7,7 @@ namespace Application.Abstractions;
 
 public interface IIntegrationRepository
 {
-    Task<Integration?> GetIntegrationByCode(string code, CancellationToken cancellationToken);
+    Task<Integration?> GetIntegrationByTelegramId(string telegramId, CancellationToken cancellationToken);
     Task<TelegramIntegration?> GetTelegramIntegration(Guid userId, CancellationToken cancellationToken);
     Task<Result> AddTelegramIntegration(TelegramIntegration integration, CancellationToken cancellationToken);
     Task<Result> ConfirmTelegramIntegration(string integrationCode, CancellationToken cancellationToken);
