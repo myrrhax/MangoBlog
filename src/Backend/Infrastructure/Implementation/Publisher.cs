5c42f10e8f5db9951be551a09d2baf8f2f194da5
myrrhax <sizintsevdenis05@mail.ru>
2025-06-11 20:21:02 +0300

    publisher impl

--- src/Backend/Infrastructure/Implementation/Publisher.cs
+++ src/Backend/Infrastructure/Implementation/Publisher.cs
@@ -1,6 +1,11 @@
 ﻿using System.Text;
 using Application.Abstractions;
+using Domain.Entities;
+using Domain.Utils;
+using Domain.Utils.Errors;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 
 namespace Infrastructure.Implementation;
@@ -9,28 +14,42 @@ internal class Publisher : IQueuePublisher
 {
     private readonly IConfiguration _configuration;
     private readonly IConnection _connection;
+    private readonly ILogger<Publisher> _logger;
 
-    public Publisher(IConfiguration configuration, IConnection connection)
+    public Publisher(IConfiguration configuration, IConnection connection, ILogger<Publisher> logger)
     {
         _configuration = configuration;
         _connection = connection;
+        _logger = logger;
     }
 
-    public void PublishMessage(string id, string message)
+    public Result Publish(Publication publication)
     {
         using var channel = _connection.CreateModel();
         string exchangeName = _configuration["RabbitMq:ExchangeName"] ?? "default";
-
-        channel.ExchangeDeclare(exchangeName,
+        try
+        {
+            channel.ExchangeDeclare(exchangeName,
             type: ExchangeType.Fanout,
             durable: true,
             autoDelete: false,
             arguments: new Dictionary<string, object>());
 
-        byte[] bytes = Encoding.UTF8.GetBytes(message);
-        channel.BasicPublish(exchange: exchangeName, 
-            routingKey: "",
-            basicProperties: null,
-            body: bytes);
+            string jsonMessage = JsonConvert.SerializeObject(publication);
+            byte[] bytes = Encoding.UTF8.GetBytes(jsonMessage);
+            channel.BasicPublish(exchange: exchangeName,
+                routingKey: "",
+                basicProperties: null,
+                body: bytes);
+
+            _logger.LogInformation("Publication {} successfully published to queue", publication.PublicationId);
+            return Result.Success();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("An error occurred publish. Error: {}", ex.Message);
+
+            return Result.Failure(new FailedToPublishAMessage());
+        }
     }
 }
