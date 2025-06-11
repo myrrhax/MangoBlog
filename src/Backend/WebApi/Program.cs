8af1248d0ad26b3c17f67ef57f95756f1e79b9a5
myrrhax <sizintsevdenis05@mail.ru>
2025-06-11 20:21:15 +0300

    fix

--- src/Backend/WebApi/Program.cs
+++ src/Backend/WebApi/Program.cs
@@ -54,10 +54,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-using var scope = app.Services.CreateScope();
-var publisher = scope.ServiceProvider.GetRequiredService<IQueuePublisher>();
-publisher.PublishMessage("1123", "Привет!");
-
 app.UseCors("AllowClient");
 app.UseHttpsRedirection();
 
