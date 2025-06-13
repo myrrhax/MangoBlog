c241653ea6e5c10ea6af6dd38c3707ed131f104e
myrrhax <sizintsevdenis05@mail.ru>
2025-06-13 21:54:57 +0300

    logic rework

--- src/Backend/Application/Integrations/Commands/ConfirmPublicationCommand.cs
+++ src/Backend/Application/Integrations/Commands/ConfirmPublicationCommand.cs
@@ -1,33 +1,46 @@
 ﻿using Application.Abstractions;
+using Application.Extentions;
+using Domain.Enums;
 using Domain.Utils;
 using Domain.Utils.Errors;
+using FluentValidation;
 using MediatR;
 
 namespace Application.Integrations.Commands;
 
 public record ConfirmPublicationCommand(Guid UserId,
     string PublicationId,
-    string RoomId) : IRequest<Result>;
+    string RoomId,
+    string IntegrationType) : IRequest<Result>;
 
 public class ConfirmPublicationCommandHandler : IRequestHandler<ConfirmPublicationCommand, Result>
 {
     private readonly IPublicationsRepository _publicationsRepository;
+    private readonly IValidator<ConfirmPublicationCommand> _validator;
 
-    public ConfirmPublicationCommandHandler(IPublicationsRepository publicationsRepository)
+    public ConfirmPublicationCommandHandler(IPublicationsRepository publicationsRepository,
+        IValidator<ConfirmPublicationCommand> validator)
     {
         _publicationsRepository = publicationsRepository;
+        _validator = validator;
     }
 
     public async Task<Result> Handle(ConfirmPublicationCommand request, CancellationToken cancellationToken)
     {
-        Result<bool> isConfirmed = await _publicationsRepository.GetConfirmationStatus(request.UserId, request.PublicationId, request.RoomId);
+        var validationResult = _validator.Validate(request);
+        if (validationResult.Errors.Any())
+        {
+            var error = new ApplicationValidationError(validationResult.Errors.ToErrorsDictionary());
+
+            return Result.Failure(error);
+        }
+
+        IntegrationType type = StringParsing.ParseIntegrationType(request.IntegrationType);
+        Result isConfirmed = await _publicationsRepository.IsStatusUnconfirmed(request.UserId, request.PublicationId, request.RoomId, type);
 
         if (isConfirmed.IsFailure)
             return Result.Failure(isConfirmed.Error);
 
-        if (!isConfirmed.Value)
-            return Result.Failure(new PublicationAlreadyConfirmed());
-
-        return await _publicationsRepository.ConfirmPublicationStatus(request.PublicationId, request.RoomId);
+        return await _publicationsRepository.ConfirmPublicationStatus(request.PublicationId, request.RoomId, type);
     }
 }
