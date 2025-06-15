9f320fa1ef65d1f504ce1b1a1b94a7df7802b508
myrrhax <sizintsevdenis05@mail.ru>
2025-06-15 17:45:30 +0300

    display publication statuses

--- src/frontend/src/components/publications/Publication.jsx
+++ src/frontend/src/components/publications/Publication.jsx
@@ -1,23 +1,66 @@
-﻿import {Box, Divider, Paper, Typography} from "@mui/material";
+﻿import {Box, Card, Divider, List, ListItem, Paper, Typography, Checkbox} from "@mui/material";
 import ImageSlider from "../ImageSlider.jsx";
 import {mediaService} from "../../services/mediaService.js";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import parseDateTime from "../../utils/parseDateTime.js";
+import TelegramIcon from "@mui/icons-material/Telegram";
 
-const Publication = ({content, medias}) => {
+const Publication = ({content, medias, creationDate, integrations}) => {
     const imageUrls = medias.map((media) => mediaService.makeImageUrl(media.id));
+    const integrationTypeIcons = {
+        'Telegram': <TelegramIcon sx={{width: 32, height: 32}} />,
+    }
     return (
         <Paper
-            sx={{display: 'flex', flexDirection: 'column'}}
+            sx={{display: 'flex', flexDirection: 'column', my: 2}}
+            elevation={3}
         >
-            <Typography variant="h6">
-                {content}
-            </Typography>
-            <Divider />
             <Box>
                 {imageUrls.length > 0 && (
                     <ImageSlider images={imageUrls} />
                 )}
             </Box>
+            <Box sx={{p: 2}}>
+                <Typography variant="h6">
+                    {content}
+                </Typography>
+                <Divider sx={{my: 2}} />
+                <Box sx={{display: 'flex', alignItems: 'center', gap: 1}}>
+                    <CalendarTodayIcon />
+                    <Typography variant="body1">
+                        {parseDateTime(creationDate)}
+                    </Typography>
+                </Box>
+                <Card sx={{my: 2, p: 2}}>
+                    {integrations.map((integration) => (
+                        <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start'}}>
+                            <Box key={integration.integrationType} sx={{display: 'flex', alignItems: 'center', gap: 1}}>
+                                {integrationTypeIcons[integration.integrationType]}
+                                <Typography sx={{fontWeight: 'bold', fontSize: 24}}>
+                                    {integration.integrationType}
+                                </Typography>
+                            </Box>
+                            <Divider sx={{my:2}}/>
+                            <List>
+                                {integration.roomStatuses.map(roomStatus => (
+                                    <ListItem key={roomStatus.roomId} sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 3}}>
+                                        <Typography sx={{fontWeight: 'bold', fontSize: 18}}>
+                                            {roomStatus.channelName}
+                                        </Typography>
+                                        <Box sx={{display: 'flex', alignItems: 'center'}}>
+                                            <Typography>
+                                                Статус публикации:
+                                            </Typography>
+                                            <Checkbox checked={roomStatus.isPublished} readOnly />
+                                        </Box>
 
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Box>
+                    ))}
+                </Card>
+            </Box>
 
         </Paper>
     )
--- src/frontend/src/utils/parseDateTime.js
+++ src/frontend/src/utils/parseDateTime.js
@@ -2,7 +2,7 @@
     const split = datetime.split('T');
 
     const date = split[0].replaceAll('-', '.');
-    const time = split[1].replace(/\.\d+.*$/, '');
+    const time = split[1].replace(/\.\d+.*$/, '').substring(0, 5);
 
     return date + ' ' + time;
 }
